b2c50ed4bbff1ca0f5a64d89283de930499bfef8
Rafael Moreira <rafa.siffert@gmail.com>
2023-10-04 19:30:55 -0400

    pass

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,50 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
+    if (username.upper() == "ADMIN" and password == "12345"):
+        return "Access granted"
+    else :
+        return "Access denied"
+    
+
     pass
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature == 33:
+        response = "brisk"
+    elif temperature == 55:
+        response = "a little chilly"
+    elif temperature == 99:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    
+    return f"It's {response} out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+    
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    switcher = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2,
+    }
+    
+    result = switcher.get(operation)
+    if result is None:
+        print("Invalid operation!")
+        return None
+    
+    return result
+
+
